56adc5f08bd9aba05220e79e7ca00ca311eff755
dobro12 <rlaehgudhoho@gmail.com>
2023-01-29 20:41:22 +0900

    delete and add visualizer

--- torch/test/car_goal2.sh
+++
@@ -1,4 +0,0 @@
-python main.py --name OffTRC-CarGoal2 --seed 1 --env_name Safexp-CarGoal1-v0 --train
-python main.py --name OffTRC-CarGoal2 --seed 2 --env_name Safexp-CarGoal1-v0 --train
-python main.py --name OffTRC-CarGoal2 --seed 3 --env_name Safexp-CarGoal1-v0 --train
-python main.py --name OffTRC-CarGoal2 --seed 4 --env_name Safexp-CarGoal1-v0 --train

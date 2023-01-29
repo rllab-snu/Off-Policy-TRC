2c4c001844c8bdaf8cad7da14b85d83fb6cc280a
dobro12 <rlaehgudhoho@gmail.com>
2023-01-29 20:41:22 +0900

    change car goal script

--- torch/test/car_goal2.sh
+++ torch/test/car_goal2.sh
@@ -1,4 +1,4 @@
-python main.py --name OffTRC-CarGoal2 --seed 1 --env_name Safexp-CarGoal1-v0 --test
-python main.py --name OffTRC-CarGoal2 --seed 2 --env_name Safexp-CarGoal1-v0 --test
-python main.py --name OffTRC-CarGoal2 --seed 3 --env_name Safexp-CarGoal1-v0 --test
-python main.py --name OffTRC-CarGoal2 --seed 4 --env_name Safexp-CarGoal1-v0 --test
+python main.py --name OffTRC-CarGoal2 --seed 1 --env_name Safexp-CarGoal1-v0
+python main.py --name OffTRC-CarGoal2 --seed 2 --env_name Safexp-CarGoal1-v0
+python main.py --name OffTRC-CarGoal2 --seed 3 --env_name Safexp-CarGoal1-v0
+python main.py --name OffTRC-CarGoal2 --seed 4 --env_name Safexp-CarGoal1-v0

7ef040f50662e66885429f31a866083fc4243c3e
dobro12 <rlaehgudhoho@gmail.com>
2023-01-29 20:41:22 +0900

    modify script file

--- torch/test/car_goal2.sh
+++ torch/test/car_goal2.sh
@@ -1,1 +1,4 @@
 python main.py --name OffTRC-CarGoal --seed 1 --env_name Safexp-CarGoal1-v0 --test
+python main.py --name OffTRC-CarGoal --seed 2 --env_name Safexp-CarGoal1-v0 --test
+python main.py --name OffTRC-CarGoal --seed 3 --env_name Safexp-CarGoal1-v0 --test
+python main.py --name OffTRC-CarGoal --seed 4 --env_name Safexp-CarGoal1-v0 --test
